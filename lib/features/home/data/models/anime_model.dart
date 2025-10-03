9bced97188df9ae7c2aa0fff28128aeb8eef588e
MostafaSensei106 <wwmostafagmer100@gmail.com>
2025-10-03 21:08:03 +0300

    Remove files ignored by .gitignore

---
+++ lib/features/home/data/models/anime_model.dart
@@ -0,0 +1,13 @@
+class AnimeModel {
+
+  AnimeModel({
+    required this.title,
+    required this.genre,
+    required this.rating,
+    required this.imageUrl,
+  });
+  final String title;
+  final String genre;
+  final double rating;
+  final String imageUrl;
+}
